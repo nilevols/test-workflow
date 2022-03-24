0e2dd3c12ef8be38748c92129ddc72d7efd36b69
Shen <296499877@qq.com>
2022-03-24 11:17:06 +0800

    test multiple files

---
+++ src/main/java/ink/dri/testworkflow/TestUtils4.java
@@ -0,0 +1,26 @@
+package ink.dri.testworkflow;
+
+/**
+ * class description
+ *
+ * @author ShenYang
+ */
+public class TestUtils4 {
+
+  public boolean checkContainsX(String text) {
+    for (char c : text.toCharArray()) {
+      if (c == 'x' || c == 'X')
+        return true;
+    }
+    return false;
+  }
+
+  public boolean checkContainsY(String text) {
+    for (char c : text.toCharArray()) {
+      if (c == 'y' || c == 'Y')
+        return true;
+    }
+    return false;
+  }
+
+}
